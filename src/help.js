71652a2e8084826b4b34ceb45f7068067274e169
Dansurvivordc <88775221+Dansurvivordc@users.noreply.github.com>
2021-08-16 07:04:19 +0000

    Update help.js

--- src/help.js
+++ src/help.js
@@ -1,13 +1,13 @@
 const help = (prefix) => {
 	return `
 
-⌜ *ѕнαηвσт ву ѕнαη∂υу* ⌟  
+⌜ *sυrѵıвσт ву ѕͩυͨrѵᎥѵor* ⌟  
 
 ◉ *INFORMACION*
    ○ Comando: ⌜ ${prefix} ⌟
-   ○ Creador: ѕнαη∂υу™ 
-   ○ Como instalar el bot: https://www.youtube.com/watch?v=2LQSzEbpJ-M
-   ○ Instagram: https://www.instagram.com/thepavos
+   ○ Creador: ѕͩυͨrѵᎥѵor™ 
+   ○ Mi canal: no tengo pa
+   ○ Instagram: https://www.instagram.com/notengo
 
 
 ◉ *NUEVOS COMANDOS*
@@ -17,8 +17,6 @@ Level para tus grupos
 Su nombre lo dice todo (antilink)
 ○ ${prefix}otak
 Monas chinas
-○ ${prefix}shantera
-Interactua con el bot
 
 ◉ *NUEVOS MENUS*
 ○ ${prefix}juegos
@@ -26,17 +24,14 @@ Divierte con tus amigos :)
 ○ ${prefix}desmenu
 Descargar musica y videos de YT
 ○ ${prefix}version
-Conoce la versión de tu bot
+Conoce la versión de survibot
 ○ ${prefix}welmenu
 Comandos de bienvedia a grupos
 
 ◉ *PARA USAR EL BOT*
 Registrate con el comando ${prefix}daftar y tu nombre
 
-◉ *RESUELVE TUS DUDAS*
-  ║
-  ╠ ○ ${prefix}creador
-  ╚ Dudas o problemas aqui
+
 
 ◉ *CREAR STICKERS*
   ║
@@ -58,19 +53,8 @@ Registrate con el comando ${prefix}daftar y tu nombre
   ╠ ○ ${prefix}idioma
   ╚ ○ ${prefix}tts es (mas texto)
 
-◉ *OTROS*
-  ║
-  ╠ ○ ${prefix}wame
-  ╠ Link de Whatsapp
-  ╠ ○ ${prefix}qrcode
-  ╚ Coloca un texto
-
 ◉ *GRUPOS*
   ║
-  ╠ ○ ${prefix}closegc
-  ╠ Cerrar el grupo solo admins
-  ╠ ○ ${prefix}opengc
-  ╠ Abrir grupo solo admins
   ╠ ○ ${prefix}kickmenu
   ╠ Eliminar a un miembro 
   ╠ ○ ${prefix}promote
@@ -82,7 +66,7 @@ Registrate con el comando ${prefix}daftar y tu nombre
   ╠ ○ ${prefix}todos
   ╚ Nombra a todos los del grupo
 
-Para usar estas funciones el bot necesita admin
+Para usar estas funciones survibot necesita admin
   
 ◉ *NSWF* 
   ║
@@ -91,13 +75,12 @@ Para usar estas funciones el bot necesita admin
 Para activar los NSFW coloque el siguiente comando ${prefix}nsfw 1 y para desactivar los NSFW coloque ${prefix}nsfw 0
 
 
-No te olvides de seguirme en instagram flaco ;)
+No te olvides de seguirme en instagram aúnque no tenga ;)
 
 
-ву ѕнαη∂υу
+ву  ѕͩυͨrѵᎥѵor  
 
-
-⌜ *ごきげんよう :)* ⌟ 
+⌜ *ごきげんよう サバイバー :V* ⌟ 
 `
 }
 
