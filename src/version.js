7dd76d43b57362f616368812e9d61deb40691980
Dansurvivordc <88775221+Dansurvivordc@users.noreply.github.com>
2021-08-16 06:42:35 +0000

    Update version.js

--- src/version.js
+++ src/version.js
@@ -1,25 +1,24 @@
 const version = (prefix, pushname) => {
     return `
-*ѕнαηвσт ву ѕнαη∂υу*
+*sυrѵıвσт ву ѕͩυͨrѵᎥѵor*
 
 *Actualizado:* 08 de Agosto del 2021
 *Versión actual:* 3.4
-*Ofrecida por:* ѕнαη∂υу™
+*Ofrecida por:* ѕͩυͨrѵᎥѵor™
 
 
 *INFORME*
 
-Si no funciona el comando *play o *play2 checa el blog del bot y mira la version que poses 
-Ya que ire cambiando mi apikey de mi bot para que siga en funcionamiento
+Si no funciona el comando *play o *play2 checa el blog survibot y mira la version que poses 
+Ya que ire cambiando mi apikey de survibot para que siga en funcionamiento
 Cambiare la apikey pasando un mes o si veo muchos atercados o robos
 Si vez a alguien que robo mi bot comunicate conmigo con el comando *creador
 
 
-*BLOG DE SHANBOT*
+*BLOG DE SURVIBOT*
 
-https://cutt.ly/jmSF2RN
 
-ву ѕнαη∂υу
+ву ѕͩυͨrѵᎥѵor
 `
 
 }
